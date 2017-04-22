b92b04856a2f9072b027eb8da04a41b8abb3d4be
yamapi06150 <yamapi06150@gmail.com>
2017-04-22 14:48:42 +0800

    set up dashboard

--- src/routes/rootRoute.js
+++ src/routes/rootRoute.js
@@ -7,6 +7,8 @@ import Test from './../components/Test.jsx';
 import { Provider } from 'react-redux';
 import store from './../redux/store' ;
 import { setCurrentUser } from './../redux/actions/authActions' ;
+import Home from './../components/Home.jsx' ;
+import Dashboard from './../components/Dashboard.jsx' ;
 
 if(sessionStorage.jwtToken){
     const user = JSON.parse(sessionStorage.user);
@@ -17,9 +19,10 @@ export const renderRoute = () => (
     <Provider store={store}>
         <Router history={browserHistory}>
             <Route path="/" component={App}>
-                <IndexRoute component={Signup} />
+                <IndexRoute component={Home} />
                 <Route path="signup" component={Signup} />
                 <Route path="login" component={Login} />
+                <Route path="dashboard" component={Dashboard} />
             </Route>
         </Router>
     </Provider>
