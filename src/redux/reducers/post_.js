a38a4680f3e408f7890f2f51f931e948f38f9ab5
yamapi06150 <yamapi06150@gmail.com>
2017-04-30 22:33:45 +0800

    fix state initial value type error

--- src/redux/reducers/post_.js
+++ src/redux/reducers/post_.js
@@ -1,6 +1,6 @@
 import { LOAD_POST } from './../actions/postActions';
 
-export default (state=[], action={}) => {
+export default ( state={}, action={} ) => {
     switch(action.type){
         case LOAD_POST:
             return action.post ;
