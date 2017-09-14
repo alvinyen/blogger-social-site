cb252c568c0b59f482c46dbe29b7399a11628567
alvinyen <103356015@nccu.edu.tw>
2017-09-14 17:26:45 +0800

    adjust display with text-overflow

--- src/components/AnnouncementBox.jsx
+++ src/components/AnnouncementBox.jsx
@@ -4,23 +4,26 @@ const getStyles = () => ({
     announcementBox: {
         fontFamily: '微軟正黑體',
         margin: '0px',
+        padding: '0 10px',
         lineHeight: '30px',
         height: '30px',
         background: 'red',
         textAlign: 'center',
         color: 'white',
         minWidth: '400px',
-        overflow: 'hidden'
+        overflow: 'hidden',
+        textOverflow: 'ellipsis',
+        whiteSpace: 'nowrap',
     }
 }) ;
 
 const AnnouncementBox = (props) => {
-    const styles = getStyles() ;
+    const styles = getStyles();
     return(
-        <div style={styles.announcementBox}>
+        <div style={styles.announcementBox} title="請使用帳號alvinyen及密碼cestlavi登入系統，以測試管理員才有的增刪改等功能">
             請使用帳號alvinyen及密碼cestlavi登入系統，以測試管理員才有的增刪改等功能
         </div>
     );
-} ;
+};
 
-export default AnnouncementBox ;
+export default AnnouncementBox;
